c01b248327169f1b3f6a4d883034a623b5c364c5
LukasKusmirek <65299144+LukasKusmirek@users.noreply.github.com>
2020-10-30 19:10:57 +0100

    added_makro_folder

---
+++ Makro/mail/contact_me.php
@@ -0,0 +1,24 @@
+<?php
+// leere felder überprüfen
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// email erstellen und absenden
+$to = 'Lukas.Kusmirek@web.de'; 
+
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // email adresse der generierten mail 
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
