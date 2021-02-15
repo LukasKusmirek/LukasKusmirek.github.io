2d033a0844914a5771023bbd7cf1daabd6c15f26
LukasKusmirek <65299144+LukasKusmirek@users.noreply.github.com>
2021-02-15 17:53:42 +0100

    Add files via upload

---
+++ Infografik/button.js
@@ -0,0 +1,35 @@
+function showTreib() {
+    var x = document.getElementById("THG");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function showNO() {
+    var x = document.getElementById("THG");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function showPm() {
+    var x = document.getElementById("PM");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function shwoKul() {
+    var x = document.getElementById("KULMEN");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
